6658eb7dd1eb65f0534546d33883b2a381ba280e
CHOI_HJ <66352570+qwas15788hj@users.noreply.github.com>
2022-02-21 13:37:39 +0900

    Time: 201 ms (44.05%), Space: 14.9 MB (48.64%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -2,14 +2,6 @@ from collections import Counter
 class Solution(object):
     def majorityElement(self, nums):
         cnt = Counter(nums).most_common()
-        print(cnt[0])
         for i in cnt:
             if i[1] > int(len(nums)/2):
                 return i[0]
-        
-        
-        # set_nums = set(nums)
-        # # for i in range(len(set_nums)):
-        # #     if nums.count(set_nums[i]) > int(len(nums)/2):
-        # #         return set_nums[i]
-        # print(set(nums)[0])
